9b9ae786dfa39e820c7d4ec48e01be1a3f30ca37
Daniela <daniela.zhao4@gmail.com>
2024-11-13 14:27:10 +0100

    ft: add create service button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
+enum BUTTON_STYLES {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary'
+}
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, MatButtonModule, CommonModule, MatIconModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
+
 export class ButtonComponent {
+
+  @Input() label: string = '';
+  @Input() style: BUTTON_STYLES = BUTTON_STYLES.PRIMARY;
+  @Input() icon: string = '';
+  @Output() onClick: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  handleButtonClick() {
+    this.onClick.emit();
+  }
 
 }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,16 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, booleanAttribute, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import Service from '../../utils/interfaces/service';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from "../input/input.component";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, TranslateModule, InputComponent],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, TranslateModule, InputComponent, ButtonComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
@@ -25,8 +26,12 @@ export class TableComponent implements AfterViewInit {
   @Input() set serviceData(value: Service[]) {
     this.dataSource = new MatTableDataSource(value);
   }
-
   @Input() displayedColumns: string[] = [];
+
+  @Input({transform: booleanAttribute}) showCreateButton: boolean = true;
+  @Input() createButtonText: string = 'LABELS.NEW';
+  @Input() createButtonIcon: string = 'add';
+  @Output() onCreateButtonClick: EventEmitter<any> = new EventEmitter();
 
   dataSource = new MatTableDataSource<Service>([]);
 
@@ -44,6 +49,11 @@ export class TableComponent implements AfterViewInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  handleCreateButtonClick() {
+    console.log("tasdf")
+    this.onCreateButtonClick.emit();
   }
 
 }
